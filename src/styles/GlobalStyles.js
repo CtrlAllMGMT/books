3d032c1141b4dcbd0368dad09244825b0aa5e85d
CtrlAllMGMT <168327674+CtrlAllMGMT@users.noreply.github.com>
2024-05-01 13:44:56 +0530

    Update GlobalStyles.js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,6 +2,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
   * {
     box-sizing: border-box;
     margin: 0;
